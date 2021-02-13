0087ad3b081475a3d00a8549fe866c792624bd3b
ivopereira-jr <ivopereira.jr12@gmail.com>
2021-02-13 16:58:08 -0300

    public

---
+++ public/scripts/addField.js
@@ -0,0 +1,25 @@
+// Procurar o botao
+document.querySelector('#add-time')
+//quando clicar no botao
+.addEventListener('click', cloneField)
+
+//executar uma acao
+function cloneField(){
+    console.log("Cheguei aqui!")
+    //duplicar os campos. Que campos?
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+
+    //pegar os campos: que campos?
+    const fields = newFieldContainer.querySelectorAll('input')
+    
+    //para cada campo, limpar
+    fields.forEach(function(field){
+        //pegar o field do momento e limpa ele
+        field.value = ""
+    })   
+
+    //colocar na pagina: onde?
+    document.querySelector('#schedule-items').appendChild(newFieldContainer)
+}
+
+   
