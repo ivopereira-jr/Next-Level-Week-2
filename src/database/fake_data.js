c5371c87a79de8d63c280a8ae8db011750126e05
ivopereira-jr <ivopereira.jr12@gmail.com>
2021-02-13 16:57:51 -0300

    src

---
+++ src/database/fake_data.js
@@ -0,0 +1,36 @@
+const proffys = [
+    {
+        name: "Diego Fernandes",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "89989987576",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "20",
+        weekday:[0],
+        time_from:[720] ,
+        time_to: [1220]
+    },
+    {
+        name: "Daniele Evangelista",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "89989987576",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "20",
+        weekday:[1],
+        time_from:[720] ,
+        time_to: [1220]
+    },
+    {
+        name: "Thiago Pederzolli",
+        avatar: "https://avatars0.githubusercontent.com/u/64425083?s=460&u=a5f4ff6ad5be91226327615ea4a96a702b0fa807&v=4",
+        whatsapp: "89989987576",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "20",
+        weekday:[1],
+        time_from:[720] ,
+        time_to: [1220]
+    }
+    
+]
